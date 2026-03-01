5dcd84d8a5124619753145c7827a4be8ac72777e
Prince Kumar <104389486+atprincekumar@users.noreply.github.com>
2026-03-01 22:48:20 +0530

    Add files via upload

---
+++ src/components/msg/Message.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Message.css";
+
+const Message = (props) => {
+
+    return (
+        <>
+            <h4>Message</h4>
+        </>
+    );
+}
+
+export default Message;
